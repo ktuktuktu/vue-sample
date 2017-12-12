055ae4e2b9f8fed581d4fa31675ccee15a3dca56
Katsumasa Oshima <katsumasa.oshima@gmail.com>
2017-12-12 11:38:51 +0900

    相対パス変更

--- my-project/config/index.js
+++ my-project/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '', // 相対パスに変更する
 
     /**
      * Source Maps
